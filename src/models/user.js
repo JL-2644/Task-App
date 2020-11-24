17909d2a9e0d4124becfacaa01d58d3483c2dc3b
Jia Qi <04jiaqi@gmail.com>
2020-11-25 11:16:11 +1300

    Fixed some corrupted files, secured password storage by hashing

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -14,7 +15,18 @@ const User = mongoose.model('User', {
         lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
-                throw new Error("Email is invalid");
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().includes('password')) {
+                throw new Error('Password cannot contain "password"')
             }
         }
     },
@@ -22,22 +34,23 @@ const User = mongoose.model('User', {
         type: Number,
         default: 0,
         validate(value) {
-            if ( value < 0) {
-                throw new Error("Age must be postitive");
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLowerCase().includes("password")) {
-                throw new Error('Password cannot contain "password"');
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     }
-});
+})
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
